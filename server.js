a69eb72dbc0a339e815d55a42fe8b457376008f7
Joseph Mwanzia <makamwanzia@gmail.com>
2024-11-22 23:47:18 +0300

    feat: convert lengths

--- server.js
+++ server.js
@@ -1,11 +1,53 @@
-const convert = require('convert-units');
-const length = document.getElementById('length-unit');
-const targetLength = document.getElementById('length-target');
-const value = document.getElementById('length-input')
-const submit = document.getElementById('submit')
+require('./index.css')
+const convert = require('convert-units')
+const convertLength = document.getElementById('convertLength');
+const tabs = document.querySelectorAll('.tab');
+const contents = document.querySelectorAll('.content');
 
-const values = convert(value).from(JSON.stringify(length)).to(JSON.stringify(targetLength))
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.remove('active'));
+        contents.forEach(c => c.classList.remove('active'));
 
-submit.addEventListener('click', () => {
-    console.log(values)
+        tab.classList.add('active');
+        document.getElementById(tab.dataset.tab).classList.add('active');
+    });
+});
+
+convertLength.addEventListener('click', (e) => {
+    e.preventDefault()
+    const input = document.getElementById('length-input').value
+    const initialUnits = document.querySelector('#length-unit').value
+    const targetUnits = document.getElementById('length-target').value;
+
+    if (!input || isNaN(input)) {
+        alert("Please enter a valid number!");
+        return;
+    }
+
+    try {
+        const measurement = convert(input).from(initialUnits).to(targetUnits)
+
+        // Select the container where results will be appended
+        const container = document.querySelector('.container')
+        const replaceTag = container.querySelector('#length');
+        const originalHTML = replaceTag.innerHTML
+
+        if (measurement) {
+            replaceTag.innerHTML =
+            `<div>
+                <h2>Results of your calculation:</h2>
+                <h1>${input}${initialUnits} = ${measurement}${targetUnits}</h1>
+            </div>
+            <button class='w-25' id='reset-button' >Reset</button>
+            `
+            const resetButton = document.querySelector('#reset-button')
+            resetButton.addEventListener('click', () => {
+                replaceTag.innerHTML = originalHTML
+            })
+
+        } 
+    } catch (error) {
+        console.error(error)
+    }
 })
